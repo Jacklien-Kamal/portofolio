819b4c35f1b814b03907b74af4364bb94ef406b5
jacklien <jacklienkamal442@gmail.com>
2024-08-02 23:44:54 +0300

    .

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -12,7 +12,7 @@ const Technologies = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-40 py-20 mx-2"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-40 py-20 mx-1"
       style={{ transform: "scale(0.9" }}
     >
       <p className="md:text-3xl mb-12 ">Making App with modern technologies</p>
